5e259db9c98b1359c9a7a99a5002ea130a2f78db
jianghe <jianghe0229@163.com>
2016-01-20 10:57:56 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/model/Province.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Province {
+	private int id ; 
+	private String privinceName;
+	private String privinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPrivinceName() {
+		return privinceName;
+	}
+	public void setPrivinceName(String privinceName) {
+		this.privinceName = privinceName;
+	}
+	public String getPrivinceCode() {
+		return privinceCode;
+	}
+	public void setPrivinceCode(String privinceCode) {
+		this.privinceCode = privinceCode;
+	}
+	
+}
